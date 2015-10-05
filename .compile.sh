329b020d93b69285ff344aef5b90eaa8c335a376
Guillem <guillem.sannicolas@keradgames.com>
2015-10-06 01:07:52 +0200

    specify include route and lib route to the compiler

--- .compile.sh
+++ .compile.sh
@@ -1,1 +1,1 @@
-cd source && g++ main.cpp -o oglprogram -lglew -lglfw3 -framework cocoa -framework opengl -framework iokit -framework corevideo
+cd source && g++ main.cpp -o oglprogram -I/usr/local/include  -L/usr/local/lib -lglew -lglfw3 -framework cocoa -framework opengl -framework iokit -framework corevideo
